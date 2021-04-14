a0e16a6cd0358008583226b98121ecb95221b30a
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-13 20:48:22 -0500

    made seed.sql for inserting initial values into database

---
+++ seed.sql
@@ -0,0 +1,13 @@
+
+USE employeeTracker_DB;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 120000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3), ("Mike", "Chan", 2, 1), ("Ashley", "Rodriguez", 3, null);
